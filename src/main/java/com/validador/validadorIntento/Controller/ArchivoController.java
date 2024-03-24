5597a68c91c393315b7d17dd589d22dcde40c560
Luzarait1 <lcanasq@unal.edu.co>
2024-03-23 19:50:25 -0500

    Initial commit

---
+++ src/main/java/com/validador/validadorIntento/Controller/ArchivoController.java
@@ -0,0 +1,36 @@
+package com.validador.validadorIntento.Controller;
+
+import com.validador.validadorIntento.Models.PersonaCsv;
+import com.validador.validadorIntento.Models.PersonaExcel;
+import com.validador.validadorIntento.Services.CSVValidator;
+import com.validador.validadorIntento.Services.ExcelValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class ArchivoController {
+
+    private CSVValidator validadorCSV;
+    private ExcelValidator validadorExcel;
+
+    @Autowired
+    public ArchivoController(CSVValidator validadorCSV) {
+        this.validadorCSV = validadorCSV;
+    }
+    @Autowired
+    public ArchivoController(ExcelValidator validadorExcel) {
+        this.validadorExcel = validadorExcel;
+    }
+
+
+    @GetMapping("/CSVValidator")
+    public Boolean validarEstructuraCSV(@RequestBody PersonaCsv registroCSV)  {
+        return this.validadorCSV.validarEstructura(registroCSV);
+    }
+
+    @GetMapping("/ExcelValidator")
+    public Boolean validarEstructuraExcel(@RequestBody PersonaExcel registroExcel) {
+        return this.validadorExcel.validarEstructura(registroExcel);
+    }
+
+}
